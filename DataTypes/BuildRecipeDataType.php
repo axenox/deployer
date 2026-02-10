e9d1dd31bfd58a2e48d1a3a90f1d918197abb0ce
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2026-02-10 08:49:42 +0100

    NEW build recipe "Build via Composer + asset FIX"

--- DataTypes/BuildRecipeDataType.php
+++ DataTypes/BuildRecipeDataType.php
@@ -38,4 +38,19 @@ class BuildRecipeDataType extends StringDataType implements EnumDataTypeInterfac
             self::CUSTOM_BUILD => 'Custom build recipe'
         ];
     }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\DataTypes\EnumDataTypeInterface::getValueHints()
+     */
+    public function getValueHints() : array
+    {
+        return [
+            self::COMPOSER_INSTALL => 'Runs a composer install on the composer.json from the build variant',
+            self::COMPOSER_INSTALL_WITH_ASSET_FIX => 'Forces asset-packagist to use its latest version cache as a workaround for different CDN problems',
+            self::CLONE_LOCAL => 'Copies the current installation instead of running composer',
+            self::CUSTOM_BUILD => 'Runs the deployer recipe file provided in the project config'
+        ];
+    }
 }
